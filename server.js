d2d9ee851497ae1f9a46656648ae10c804a72af4
Marie <marii26mp@gmail.com>
2022-03-18 19:10:32 -0400

    deploy

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://admin:Leineckerg4lp@cluster0.zx86x.mongodb.net/COP4331?retryWrites=true&w=majority';
 
+const app = express();
+const PORT = process.env.PORT || 5000;  
+app.set('port', (process.env.PORT || 5000));
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const path = require('path');           
+
+// MERN B updates this to no longer use a connection string
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
 client.connect();
 
+if (process.env.NODE_ENV === 'production') 
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+  app.get('*', (req, res) => 
+ {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
 
 app.use((req, res, next) => 
 {
@@ -87,4 +104,8 @@ app.post('/login', async (req, res, next) =>
 
 });
 
-app.listen(5000); // start Node + Express server on port 5000
+//app.listen(5000); // start Node + Express server on port 5000
+app.listen(PORT, () => 
+{
+  console.log('Server listening on port ' + PORT);
+});
