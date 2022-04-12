c69b79ffeec1dd1fedd0ab1885476217758e90fb
adabs <goddemraw@gmail.com>
2022-04-12 18:06:05 -0400

    professor path change

--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,11 @@ api.setApp( app, client );
 if (process.env.NODE_ENV === 'production') 
 {
     // Set static folder
-    app.use(express.static('frontend/build'));
+    app.use(express.static('../frontend/build'));
 
     app.get('*', (req, res) => 
     {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
     });
 }
 
