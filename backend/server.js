cf911d2c2fd4bc8d577b3a2963c45d38d1b3d67d
Marie <marii26mp@gmail.com>
2022-04-13 11:37:23 -0400

    path resolve attempt1

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'production')
 
     app.get('*', (req, res) => 
     {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'));
     });
 }
 
