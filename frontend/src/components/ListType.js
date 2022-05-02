9b65924466e626d7ce698ba750f74cb29ccf9255
Marie <marii26mp@gmail.com>
2022-05-02 18:39:41 -0400

    Fixed Logout timeout to redirect user to the Guest page upon jwToken expiration

--- frontend/src/components/ListType.js
+++ frontend/src/components/ListType.js
@@ -33,9 +33,7 @@ function ListType(props){
     ):(
         <div>
             <br></br><br></br>
-            THIS FEATURE IS<br></br><br></br>
             NOT AVAILABLE <br></br><br></br>
-            FOR FRIENDS
         </div>);
     } catch(e){return null;}
 }
